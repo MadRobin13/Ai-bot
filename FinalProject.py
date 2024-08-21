f9311112ae95bb76097d54e38c8fa06e84599a54
Abhimanyu Chaudhary <madrobin1340@gmail.com>
2024-08-21 18:39:30 +0100

    fixed eda, and started/debugged reommendation

--- FinalProject.py
+++ FinalProject.py
@@ -11,11 +11,14 @@ pd.options.display.max_rows = None
 dotenv.load_dotenv()
 
 intents = discord.Intents.default()
+intents.message_content = True
+
 
 client = discord.Client(intents=intents)
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 OPENAI_API_KEY = os.getenv('OPEN_AI_API_KEY')
+ai = openai.OpenAI(api_key=OPENAI_API_KEY)
 GENAI_ID = os.getenv('GEN_ASSISTANT_ID')
 
 
@@ -23,11 +26,37 @@ df = pd.read_csv('my_movies.csv')
 
 def eda():
     sorted_df = df.sort_values('Abhi')
-    avg = sorted_df['Abhi'].mean()
+    avg = round(sorted_df['Abhi'].mean(), 3)
     least = sorted_df['Movies'].iloc[0]
     greatest = sorted_df['Movies'].iloc[len(sorted_df) - 1]
 
     return avg, least, greatest
+
+def recommend(channel):
+    filter_condition = df['Abhi'] > df['Abhi'].quantile(0.7)
+    filtered_df = df[filter_condition]
+    # return filtered_df
+
+    messages = []
+    message = {
+        'role' : 'user',
+        'content' : 'create a a movie synopsis based on these movies I like'
+    }
+
+    thread = ai.beta.threads.create(
+        messages=messages
+    )
+
+    ai.beta.threads.runs.create(
+        assistant_id=GENAI_ID,
+        max_prompt_tokens=300,
+        max_completion_tokens=300,
+        thread_id=thread.id
+    )
+
+    response = "can't do much right now"
+    return response
+
 
 @client.event
 async def on_ready():
@@ -35,18 +64,21 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    cont = message.content.split()
-    msg = ' '.join(cont[1:]).lower()
+    cont = str(message.content)
+    msg = cont[3:].lower()
     output_message = ''
     if message.author.bot:
         return
-    await message.channel.send('hi')
-    if cont[0] == '||':
-        await message.channel.send('hello')
+    # await message.channel.send(str(message.content))
+    # await message.channel.send(msg)
+    # await message.channel.send('hi')
+    if cont.lower().startswith('||'):
         if msg == 'eda':
             avg, least, greatest = eda()
-            output_message = f"your average is {avg}, your least rated movie is {least} and your highest rated movie is {greatest}"
-        await message.channel.send(output_message)
+            output_message = f"your average rating is {avg}, your least rated movie is {least}, and your highest rated movie is {greatest}"
+            await message.channel.send(output_message)
+        if msg == 'recommend':
+            await message.channel.send(str(recommend(message.channel)))
 
 
 def main():
